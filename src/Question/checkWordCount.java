ba5ca3bfccf609b38e2d30dfc0c6eb5a6666def0
Sangram <sangramm@nedbank.co.za>
2024-12-03 21:24:02 +0530

    Program For checkWord

--- src/Question/checkWordCount.java
+++ src/Question/checkWordCount.java
@@ -3,10 +3,9 @@ package Question;
 import java.util.Scanner;
 
 public class checkWordCount {
-
     Scanner sc = new Scanner(System.in);
-
     int count = 1;
+    int countValue = 0;
 
     public void checkWord() {
 
@@ -22,8 +21,20 @@ public class checkWordCount {
         System.out.println("Word Count = " + count);
     }
 
+    public void checkWordWithTrim(String string) {
+        string = string.trim();
+        for (int i = 0; i < string.length(); i++) {
+            if (!string.isEmpty() && (string.charAt(i) == ' ') || (string.length() - 1 == i)) {
+                countValue++;
+            }
+        }
+        System.out.println("countValue : " + countValue);
+    }
+
     public static void main(String[] args) {
         checkWordCount cw = new checkWordCount();
         cw.checkWord();
+        String s = "Hellow World  ";
+        cw.checkWordWithTrim(s);
     }
 }
